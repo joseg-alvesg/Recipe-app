cee420a5d85c7a6a62294626273e131c577ed840
joseg-alvesg <cinhoalves85@gmail.com>
2023-03-02 19:08:20 -0300

    create: `inputs` and `button`

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <section>
+        login image
+      </section>
+      <section>
+        <input
+          type="text"
+          data-testid="email-input"
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+        />
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+        >
+          login
+        </button>
+      </section>
+    </div>
   );
 }
