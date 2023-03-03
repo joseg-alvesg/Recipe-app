7f54abbd9966257c8452740db50ae8122fc4d24e
joseg-alvesg <cinhoalves85@gmail.com>
2023-03-03 16:16:31 -0300

    route `login` for test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import SearchProvider from './contexts/SearchProvider';
 import Drinks from './pages/Drinks';
+import Login from './pages/Login';
 import Meals from './pages/Meals';
 
 function App() {
   return (
     <SearchProvider>
       <Switch>
+        <Route exact path="/" component={ Login } />
         <Route exact path="/meals" component={ Meals } />
         <Route exact path="/drinks" component={ Drinks } />
       </Switch>
