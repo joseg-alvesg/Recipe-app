9159f709c5a3d968553fd84e29e1f3d0151fa601
joseg-alvesg <cinhoalves85@gmail.com>
2023-03-07 21:31:12 -0300

    refact: test of `Recipes` refact: `RenderWithRouter` to use initialEntries

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,6 +1,7 @@
-import { screen, waitFor } from '@testing-library/react';
+import { renderHook, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import meals from '../../cypress/mocks/meals';
@@ -8,6 +9,9 @@ import mealCategories from '../../cypress/mocks/mealCategories';
 import drinks from '../../cypress/mocks/drinks';
 import drinkCategories from '../../cypress/mocks/drinkCategories';
 import beefMeals from '../../cypress/mocks/beefMeals';
+import ordnaryDrinks from '../../cypress/mocks/ordinaryDrinks';
+import { Recipes } from '../pages';
+import SearchProvider from '../contexts/SearchProvider';
 // import SearchProvider from '../contexts/SearchProvider';
 
 const TEST_EMAIL = 'test@test.com';
@@ -17,164 +21,134 @@ const PASSWORD_INPUT = 'password-input';
 const LOGIN_BUTTON = 'login-submit-btn';
 
 describe('Testa a página de receitas', () => {
+  beforeEach(() => {
+    const endpoints = {
+      'https://www.themealdb.com/api/json/v1/1/search.php?s=': () => Promise.resolve({ json: () => meals }),
+      'https://www.themealdb.com/api/json/v1/1/list.php?c=list': () => Promise.resolve({ json: () => mealCategories }),
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=': () => Promise.resolve({ json: () => drinks }),
+      'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list': () => Promise.resolve({ json: () => drinkCategories }),
+      'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef': () => Promise.resolve({ json: () => beefMeals }),
+      'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink': () => Promise.resolve({ json: () => ordnaryDrinks }),
+    };
+
+    jest.spyOn(global, 'fetch').mockImplementation((url) => (endpoints[url] ? endpoints[url]() : Promise.resolve(new Error('URL not found'))));
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('Testa se a página é renderizada corretamente com 5 botões a mais', async () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-      json: () => meals,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => mealCategories,
-    }));
-
-    const { history } = renderWithRouter(<App />);
-
-    const emailInput = screen.getByTestId(EMAIL_INPUT);
-    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
-    const submitButton = screen.getByTestId(LOGIN_BUTTON);
-
-    await act(async () => {
-      userEvent.type(emailInput, TEST_EMAIL);
-      userEvent.type(passwordInput, TEST_PASSWORD);
-      userEvent.click(submitButton);
-    });
+  it('Testa se a página de receitas é renderizada na rota /meals', async () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
     await waitFor(() => {
+      expect(global.fetch).toBeCalledTimes(2);
       expect(history.location.pathname).toBe('/meals');
-      expect(global.fetch).toBeCalledTimes(2);
-    });
-    const beefButton = screen.getByTestId('Beef-category-filter');
-    const chickenButton = screen.getByTestId('Chicken-category-filter');
-    const dessertButton = screen.getByTestId('Dessert-category-filter');
-    const goatButton = screen.getByTestId('Goat-category-filter');
 
-    expect(beefButton).toBeInTheDocument();
-    expect(chickenButton).toBeInTheDocument();
-    expect(dessertButton).toBeInTheDocument();
-    expect(goatButton).toBeInTheDocument();
-  });
-
-  it('Testa se as 12 comidas são renderizadas', async () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-      json: () => meals,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => mealCategories,
-    }));
-
-    const { history } = renderWithRouter(<App />);
-
-    const emailInput = screen.getByTestId(EMAIL_INPUT);
-    const passwordInput = screen.getByTestId(PASSWORD_INPUT);
-    const submitButton = screen.getByTestId(LOGIN_BUTTON);
-
-    await act(async () => {
-      userEvent.type(emailInput, TEST_EMAIL);
-      userEvent.type(passwordInput, TEST_PASSWORD);
-      userEvent.click(submitButton);
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('Corba');
     });
 
-    await waitFor(() => {
-      expect(history.location.pathname).toBe('/meals');
-      expect(global.fetch).toBeCalledTimes(2);
-    });
-
-    meals.meals.splice(0, 12).forEach((_meal, index) => {
-      const mealCard = screen.getByTestId(`${index}-recipe-card`);
-      expect(mealCard).toBeInTheDocument();
-    });
-  });
-
-  it('Testa se os botôes de categoria são renderizados', async () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-      json: () => drinks,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => drinkCategories,
-    }));
-
-    const { history } = renderWithRouter(<App />);
-
-    await act(async () => {
-      history.push('/drinks');
-    });
-
-    await waitFor(() => {
-      expect(global.fetch).toBeCalledTimes(2);
-    });
-
-    drinkCategories.drinks.splice(0, 12).forEach((drink) => {
-      const drinkButton = screen.getByTestId(`${drink.strCategory}-category-filter`);
-      expect(drinkButton).toBeInTheDocument();
-    });
-  });
-
-  it('Testa se os 12 drinks são renderizados', async () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-      json: () => drinks,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => drinkCategories,
-    }));
-
-    const { history } = renderWithRouter(<App />);
-
-    await act(async () => {
-      history.push('/drinks');
-    });
-
-    await waitFor(() => {
-      expect(global.fetch).toBeCalledTimes(2);
-    });
-
-    drinks.drinks.splice(0, 12).forEach((_drink, index) => {
-      const drinkCard = screen.getByTestId(`${index}-recipe-card`);
-      expect(drinkCard).toBeInTheDocument();
-    });
-  });
-
-  it.only('Testa se o botão de categoria de comida funciona', async () => {
-    jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.resolve({
-      json: () => meals,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => mealCategories,
-    })).mockImplementationOnce(() => Promise.resolve({
-      json: () => beefMeals,
-    }));
-
-    const { history } = renderWithRouter(<App />);
-
-    await act(async () => {
-      history.push('/meals');
-    });
-
-    await waitFor(() => {
-      expect(global.fetch).toBeCalledTimes(2);
-    });
-
-    const recipeCard0 = screen.getByTestId('0-recipe-card');
-    const recipeCard1 = screen.getByTestId('1-recipe-card');
-    expect(recipeCard0).toBeInTheDocument();
-    expect(recipeCard1).toBeInTheDocument();
-
-    expect(recipeCard0).toHaveTextContent('Corba');
-    expect(recipeCard1).toHaveTextContent('Kumpir');
-
-    const beefButton = screen.getByTestId('Beef-category-filter');
-
-    await act(async () => {
-      userEvent.click(beefButton);
-    }, 2000);
+    const beefBtn = screen.getByTestId('Beef-category-filter');
+    userEvent.click(beefBtn);
 
     await waitFor(() => {
       expect(global.fetch).toBeCalledTimes(3);
+      expect(global.fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef');
+
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('Beef and Mustard Pie');
     });
 
-    const recipeCard2 = screen.getByTestId('0-recipe-card');
-    const recipeCard3 = screen.getByTestId('1-recipe-card');
+    const allBtn = screen.getByTestId('All-category-filter');
+    userEvent.click(allBtn);
 
-    expect(recipeCard2).toBeInTheDocument();
-    expect(recipeCard3).toBeInTheDocument();
+    await waitFor(() => {
+      expect(global.fetch).toBeCalledTimes(4);
+      expect(global.fetch).toBeCalledWith('https://www.themealdb.com/api/json/v1/1/search.php?s=');
 
-    expect(recipeCard2).toHaveTextContent('Beef and Mustard Pie');
-    expect(recipeCard3).toHaveTextContent('Beef and Oyster pie');
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      const cardRecipe1 = screen.getByTestId(`${1}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe1).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('Corba');
+      expect(cardRecipe1).toHaveTextContent('Kumpir');
+    });
+  });
+
+  it('Testa se a página de receitas é renderizada na rota /drinks', async () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/drinks'] });
+
+    await waitFor(() => {
+      expect(global.fetch).toBeCalledTimes(2);
+      expect(history.location.pathname).toBe('/drinks');
+
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('GG');
+    });
+
+    const ordinaryBtn = screen.getByTestId('Ordinary Drink-category-filter');
+    userEvent.click(ordinaryBtn);
+
+    await waitFor(() => {
+      expect(global.fetch).toBeCalledTimes(3);
+      expect(global.fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink');
+
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('3-Mile Long Island Iced Tea');
+    });
+
+    const allBtn = screen.getByTestId('All-category-filter');
+    userEvent.click(allBtn);
+
+    await waitFor(() => {
+      expect(global.fetch).toBeCalledTimes(4);
+      expect(global.fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+
+      const cardRecipe0 = screen.getByTestId(`${0}-recipe-card`);
+      const cardRecipe1 = screen.getByTestId(`${1}-recipe-card`);
+      expect(cardRecipe0).toBeInTheDocument();
+      expect(cardRecipe1).toBeInTheDocument();
+      expect(cardRecipe0).toHaveTextContent('GG');
+      expect(cardRecipe1).toHaveTextContent('A1');
+    });
+  });
+
+  it('não chama setRecipes para pathname desconhecido', async () => {
+    renderWithRouter(<SearchProvider><Recipes /></SearchProvider>, { initialEntries: ['/unknown'] });
+
+    await waitFor(() => {
+      expect(global.fetch).not.toBeCalled();
+    });
+
+    const cardRecipe0 = screen.queryByTestId(`${0}-recipe-card`);
+    expect(cardRecipe0).not.toBeInTheDocument();
+  });
+
+  it('Testa o botão all numa tela inexistente', async () => {
+    const handleClick = jest.fn();
+    renderWithRouter(<SearchProvider><Recipes /></SearchProvider>, { initialEntries: ['/unknown'] });
+
+    await waitFor(() => {
+      expect(global.fetch).not.toBeCalled();
+    });
+
+    const allBtn = screen.queryByTestId('All-category-filter');
+    expect(allBtn).toBeInTheDocument();
+
+    userEvent.click(allBtn);
+
+    await waitFor(() => {
+      expect(global.fetch).not.toBeCalled();
+
+      expect(handleClick).not.toBeCalled();
+
+      const cardRecipe0 = screen.queryByTestId(`${0}-recipe-card`);
+      expect(cardRecipe0).not.toBeInTheDocument();
+    });
   });
 });
