3ac16227f19ee23a85c87eff5408a5d2c47be500
Felipe ¨GrYllO <GrYllO@users.noreply.github.com>
2023-03-13 18:47:11 -0300

    [MAIN GROUP 16] feature: NotFound page (#629)

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import jacquinPessimo from '../../images/jacquinPessimo.jpg';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Header title="404" renderSearchBtn={ false } />
+      <section>
+        <h2>
+          {`It's not too bad, but it's too bad:
+          the page you've requested is not found.`}
+        </h2>
+        <img
+          alt="Erick Jacquin saying It's not too bad, but it's too bad"
+          src={ jacquinPessimo }
+        />
+      </section>
+      <Footer />
+    </>
   );
 }
